18d99eedd024d24a1e8dacfb774a0c5cd849dcdf
seondal <sseondal@gmail.com>
2024-02-08 13:28:55 +0900

    💄 style :  연락책 업데이트

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,19 +6,19 @@ export default function Page() {
       <h4>Feel Free to Contact</h4>
       <ul>
         <li>
-          <a href={`mailto:${Contact.email}`}>{Contact.email}</a>
+          이메일 : <a href={`mailto:${Contact.email}`}>{Contact.email}</a>
         </li>
         <li>
-          개인 이메일 :
+          학회장 개인 이메일 :{" "}
           <a href={`mailto:${Contact.personal_email}`}>
             {Contact.personal_email}
           </a>
         </li>
         <li>
-          <a href={Contact.instagram}>Instagram</a>
+          Instagram :<a href={Contact.instagram}>@ewha.chain</a>
         </li>
         <li>
-          <a href={Contact.twitter}>X</a>
+          X (Twitter) :<a href={Contact.twitter}>@Ewhachain</a>
         </li>
       </ul>
     </div>
