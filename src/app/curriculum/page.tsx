899a155e75596c4e4269e7beb3ce88b60cea77e4
seondal <sseondal@gmail.com>
2024-02-08 13:41:01 +0900

    ✨ feat :  커리큘럼 내용 추가

--- src/app/curriculum/page.tsx
+++ src/app/curriculum/page.tsx
@@ -1,3 +1,9 @@
+import MarkdownViewer from "@/utils/markdownViewer";
+
 export default function Page() {
-  return <div>기초세션, 심화세션, 아이디어컨테스트</div>;
+  return (
+    <div>
+      <MarkdownViewer file="curriculum" />
+    </div>
+  );
 }
